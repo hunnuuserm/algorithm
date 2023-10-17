ccc77fafb51fdfb1db523e8d95e5669be1f2d097
mm <3072764440@qq.com>
2023-10-17 23:40:56 +0800

    提交第二次做的结果

--- optimizationAlgorithm/滑动窗口/lengthOfLongestSubstring.cpp
+++ optimizationAlgorithm/滑动窗口/lengthOfLongestSubstring.cpp
@@ -4,6 +4,28 @@
 //解题思路：hash映射+滑动窗口
 //判断一个字符是否与前面的字符重合时，不用特意写一个函数，用hash映射即可轻松解决
 
+//第二次做:
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int ret = INT_MIN;
+        int left = 0, right = 0, n = s.size();
+        int hash[128] = { 0 };
+        while (right < n)
+        {
+            hash[s[right]]++;//进窗口
+            while (hash[s[right]] > 1)//判断
+            {
+                hash[s[left++]]--;//出窗口
+            }
+            ret = max(ret, right - left + 1);//更新结果
+            right++;
+        }
+        return ret == INT_MIN ? 0 : ret;
+    }
+};
+
+//第一次做：
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
--- optimizationAlgorithm/滑动窗口/longestOnes.cpp
+++ optimizationAlgorithm/滑动窗口/longestOnes.cpp
@@ -1,6 +1,28 @@
 //https://leetcode.cn/problems/max-consecutive-ones-iii/
 //leetcode 1004. 最大连续1的个数 III
 //滑动窗口
+
+//第二次做，代码写的很简洁，记录一下:
+class Solution {
+public:
+    int longestOnes(vector<int>& nums, int k) {
+        int left = 0, right = 0, n = nums.size();
+        int ret = 0;
+        while (right < n)
+        {
+            if (nums[right] == 0) k--;//进窗口
+            while (k < 0)//判断
+            {
+                if (nums[left++] == 0) k++;//出窗口
+            }
+            ret = max(ret, right - left + 1);//更新结果
+            right++;
+        }
+        return ret;
+    }
+};
+
+//第一次做：
 class Solution {
 public:
     int longestOnes(vector<int>& nums, int k) {
